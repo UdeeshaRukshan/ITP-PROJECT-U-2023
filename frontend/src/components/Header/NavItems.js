661febbc6399e8c1d651cb54ef71904d225998fa
UdeeshaRukshan <IT21807480@my.sliit.lk>
2023-08-30 00:01:31 +0530

    copy files from local project without installing npm packages
     i created sign in page with the navigation bar but didn't chek its working or not

---
+++ frontend/src/components/Header/NavItems.js
@@ -0,0 +1,53 @@
+export const navItems = [
+  {
+    id: 1,
+    title: "Vehicle",
+    path: "/home",
+    cName: "nav-item",
+  },
+  {
+    id: 2,
+    title: "Properties",
+    path: "/about",
+    cName: "nav-item",
+  },
+  {
+    id: 3,
+    title: "Collectables",
+    path: "./pages/Products",
+    cName: "nav-item",
+  },
+  {
+    id: 4,
+    title: "Arts",
+    path: "./pages/Blog.js",
+    cName: "nav-item",
+  },
+];
+
+export const serviceDropdown = [
+  {
+    id: 1,
+    title: "Vehicle",
+    path: "/home",
+    cName: "submenu-item",
+  },
+  {
+    id: 2,
+    title: "Properties",
+    path: "./pages/Profile",
+    cName: "submenu-item",
+  },
+  {
+    id: 3,
+    title: "Collectables",
+    path: "./pages/Products",
+    cName: "submenu-item",
+  },
+  {
+    id: 4,
+    title: "Arts",
+    path: "./pages/Blog",
+    cName: "submenu-item",
+  },
+];
---
+++ frontend/src/pages/Blog.js
@@ -0,0 +1,5 @@
+import React from "react";
+function Blog() {
+  return <div>This is my Wishlist</div>;
+}
+export default Blog;
