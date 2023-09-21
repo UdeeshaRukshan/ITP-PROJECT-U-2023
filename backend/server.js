8a0bd681dac17a14f9915a3ee4d4dc140792b4c4
Sahan <randikasahan652@gmail.com>
2023-09-21 15:44:55 +0530

    states

--- backend/server.js
+++ backend/server.js
@@ -26,7 +26,7 @@ app.listen(PORT, () => {
 
 app.use(
   cors({
-    origin: ["http://localhost:3004"],
+    origin: ["http://localhost:3000"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     //optionsSuccessStatus: 200,
     credentials: true,
