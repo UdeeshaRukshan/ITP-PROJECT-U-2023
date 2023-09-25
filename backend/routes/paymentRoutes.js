f50afa29ca295412bb6206669b05eaa4ce7012ee
UdeeshaRukshan <IT21807480@my.sliit.lk>
2023-09-25 22:13:28 +0530

    update thamasha

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -2,86 +2,96 @@ const router = require("express").Router();
 const Payment = require("../models/Payment");
 
 // data insert route
-router.route("/add").post((req,res)=>{
-
+router.route("/add").post((req, res) => {
   const cardName = req.body.cardName;
   const cardNumber = req.body.cardNumber;
   const expiryDate = req.body.expiryDate;
   const cvv = req.body.cvv;
 
   const newPayment = new Payment({
+    cardName,
+    cardNumber,
+    expiryDate,
+    cvv,
+  });
 
-      cardName,
-      cardNumber,
-      expiryDate,
-      cvv
-  })
-
-  newPayment.save().then(()=>{
-      res.json("Payment details Added")
-  }).catch((err)=>{
+  newPayment
+    .save()
+    .then(() => {
+      res.json("Payment details Added");
+    })
+    .catch((err) => {
       console.log(err);
-  })
-
-})
+    });
+});
 
 // data read route
-router.route("/").get((req,res)=>{
-
-  Payment.find().then((paymentRoutes)=>{
-      res.json(paymentRoutes)
-  }).catch((err)=>{
-      console.log(err)
-  }) 
-})
-
+router.route("/").get((req, res) => {
+  Payment.find()
+    .then((paymentRoutes) => {
+      res.json(paymentRoutes);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 // data update route
-router.route("/update/:id").put(async (req,res) => {
-  let userId = req.params.id; 
-  const {cardName, cardNumber, expiryDate, cvv} = req.body;
+router.route("/update/:id").put(async (req, res) => {
+  let userId = req.params.id;
+  const { cardName, cardNumber, expiryDate, cvv } = req.body;
 
   const updatePayment = {
     cardName,
     cardNumber,
     expiryDate,
-    cvv
-  }
+    cvv,
+  };
 
   const update = await Payment.findByIdAndUpdate(userId, updatePayment)
-  .then(() => {
-      res.status(200).send({status: "Payment details updated"})
-  }).catch((err) => {
+    .then(() => {
+      res.status(200).send({ status: "Payment details updated" });
+    })
+    .catch((err) => {
       console.log(err);
-      res.status(500).send({status: "Error with updating data", error: err.message});
-  })
-
-})
-
+      res
+        .status(500)
+        .send({ status: "Error with updating data", error: err.message });
+    });
+});
 
 // data delete route
 router.route("/delete/:id").delete(async (req, res) => {
   let userId = req.params.id;
 
   await Payment.findByIdAndDelete(userId)
-      .then(() => {
-          res.status(200).send({status: "Payment details deleted"});
-      }).catch((err) => {
-          console.log(err.message);
-          res.status(500).send({status: "Error with delete details", error: err.message});
-      })
-})
+    .then(() => {
+      res.status(200).send({ status: "Payment details deleted" });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res
+        .status(500)
+        .send({ status: "Error with delete details", error: err.message });
+    });
+});
 
 // fetch data route
 router.route("/get/:id").get(async (req, res) => {
   let userId = req.params.id;
   const user = await Payment.findById(userId)
-  .then((payment) => {
-      res.status(200).send({status: "Payment details fetched", payment})
-  }).catch((err) => {
+    .then((payment) => {
+      res.status(200).send({ status: "Payment details fetched", payment });
+    })
+    .catch((err) => {
       console.log(err.message);
-      res.status(500).send({status: "Error with getting Payment details", error: err.message});
-  })
-})
+      res
+        .status(500)
+        .send({
+          status: "Error with getting Payment details",
+          error: err.message,
+        });
+    });
+});
 
 module.exports = router;
--- backend/routes/shippingRoutes.js
+++ backend/routes/shippingRoutes.js
@@ -2,50 +2,58 @@ const router = require("express").Router();
 let Shipping = require("../models/Shipping");
 
 // Data insert route
-router.route("/add").post((req,res)=>{
-
+router.route("/add").post((req, res) => {
   const firstName = req.body.firstName;
   const lastName = req.body.lastName;
   const addressLine1 = req.body.addressLine1;
   const addressLine2 = req.body.addressLine2;
   const city = req.body.city;
   const province = req.body.province;
-  const postalCode = req.body.postalCode; 
+  const postalCode = req.body.postalCode;
 
   const newShipping = new Shipping({
+    firstName,
+    lastName,
+    addressLine1,
+    addressLine2,
+    city,
+    province,
+    postalCode,
+  });
 
-      firstName,
-      lastName,
-      addressLine1,
-      addressLine2,
-      city,
-      province,
-      postalCode
-  })
-
-  newShipping.save().then(()=>{
-      res.json("Shipping details Added")
-  }).catch((err)=>{
+  newShipping
+    .save()
+    .then(() => {
+      res.json("Shipping details Added");
+    })
+    .catch((err) => {
       console.log(err);
-  })
-
-})
+    });
+});
 
 //data read route
-router.route("/").get((req,res)=>{
-
-  Shipping.find().then((shippingRoutes)=>{
-      res.json(shippingRoutes)
-  }).catch((err)=>{
-      console.log(err)
-  }) 
-})
-
+router.route("/").get((req, res) => {
+  Shipping.find()
+    .then((shippingRoutes) => {
+      res.json(shippingRoutes);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 
 //data update route
-router.route("/update/:id").put(async (req,res) => {
-  let userId = req.params.id; 
-  const {firstName, lastName, addressLine1, addressLine2, city, province, postalCode} = req.body;
+router.route("/update/:id").put(async (req, res) => {
+  let userId = req.params.id;
+  const {
+    firstName,
+    lastName,
+    addressLine1,
+    addressLine2,
+    city,
+    province,
+    postalCode,
+  } = req.body;
 
   const updateShipping = {
     firstName,
@@ -54,42 +62,52 @@ router.route("/update/:id").put(async (req,res) => {
     addressLine2,
     city,
     province,
-    postalCode
-  }
+    postalCode,
+  };
 
   const update = await Shipping.findByIdAndUpdate(userId, updateShipping)
-  .then(() => {
-      res.status(200).send({status: "Shipping details updated"})
-  }).catch((err) => {
+    .then(() => {
+      res.status(200).send({ status: "Shipping details updated" });
+    })
+    .catch((err) => {
       console.log(err);
-      res.status(500).send({status: "Error with updating data", error: err.message});
-  })
-
-})
-
+      res
+        .status(500)
+        .send({ status: "Error with updating data", error: err.message });
+    });
+});
 
 //data delete route
 router.route("/delete/:id").delete(async (req, res) => {
   let userId = req.params.id;
 
   await Shipping.findByIdAndDelete(userId)
-      .then(() => {
-          res.status(200).send({status: "Shipping details deleted"});
-      }).catch((err) => {
-          console.log(err.message);
-          res.status(500).send({status: "Error with delete details", error: err.message});
-      })
-})
+    .then(() => {
+      res.status(200).send({ status: "Shipping details deleted" });
+    })
+    .catch((err) => {
+      console.log(err.message);
+      res
+        .status(500)
+        .send({ status: "Error with delete details", error: err.message });
+    });
+});
 
 //fetch data route
 router.route("/get/:id").get(async (req, res) => {
   let userId = req.params.id;
   const user = await Shipping.findById(userId)
-  .then((shipping) => {
-      res.status(200).send({status: "Shipping details fetched", shipping})
-  }).catch((err) => {
+    .then((shipping) => {
+      res.status(200).send({ status: "Shipping details fetched", shipping });
+    })
+    .catch((err) => {
       console.log(err.message);
-      res.status(500).send({status: "Error with get shipping details", error: err.message});
-  })
-})
+      res
+        .status(500)
+        .send({
+          status: "Error with get shipping details",
+          error: err.message,
+        });
+    });
+});
 module.exports = router;
