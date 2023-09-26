680ac131d3b2ea4b0847208270348327d128edce
thamashaa <thamashapasidunee105@gmail.com>
2023-09-26 13:07:32 +0530

    Update header

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,25 +2,25 @@ import React from "react";
 
 function Header() {
     return (
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#" style={{color:"red"}}>Navbar</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Home</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Features</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Categories</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Pricing</a>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Feedback</a>
               </li>
-              <li class="nav-item">
-                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+              <li className="nav-item">
+                <a className="nav-link disabled" aria-disabled="true">About</a>
               </li>
             </ul>
           </div>
@@ -30,3 +30,4 @@ function Header() {
 }
 
 export default Header;
+
