26f3a89310e3967ce6ba9de2b2edf99b2b8b35f1
UdeeshaRukshan <IT21807480@my.sliit.lk>
2023-09-28 01:26:04 +0530

    pissu hadenawa bn

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.js';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
-import { SnackbarProvider } from 'notistack';
+import ReactDOM from "react-dom/client";
+import App from "./App.js";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { SnackbarProvider } from "notistack";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <SnackbarProvider>
       <App />
