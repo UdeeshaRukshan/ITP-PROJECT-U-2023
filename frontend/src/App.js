1214a9ff6c77cf14d0a239f80d395711ed3bea07
UdeeshaRukshan <IT21807480@my.sliit.lk>
2023-09-29 03:45:46 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,10 +12,9 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import About from "./pages/About";
+
 import ContactUs from "./pages/ContactUs";
-import Blog from "./pages/Blog";
-import Profile from "./pages/Profile";
+
 import Dashboard from "./pages/Dashbord";
 
 import Checkout from "./pages/Checkout";
@@ -24,34 +23,31 @@ import SubmitTicketPage from "./pages/submitTicket/submitTicket";
 import Chatbot from "./components/support/chatBot/Chatbot";
 import { Notifications } from "@mantine/notifications";
 import MyTicketPage from "./pages/MyTickets/myTickets";
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 function App() {
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient();
   return (
     <MantineProvider withGlobalStyles withNormalizeCSS>
       <Notifications position="top-center" />
-      <QueryClientProvider client={queryClient}> 
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/dashbord" element={<Dashboard />} />
-          <Route path="/home" element={<IndexPage />} />
-          <Route path="/payment" element={<Checkout />} />
+      <QueryClientProvider client={queryClient}>
+        <div className="App">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/dashbord" element={<Dashboard />} />
+            <Route path="/home" element={<IndexPage />} />
+            <Route path="/payment" element={<Checkout />} />
 
-          <Route path="/contactUs" element={<ContactUs />} />
-          <Route path="/ticket/submit" element={<SubmitTicketPage />} />
-          <Route path="/support/chatbot" element={<Chatbot />} />
-          <Route path="/ticket/mytickets" element= {<MyTicketPage/>}/>
-        </Routes>
-        <Footer />
-      </div>
+            <Route path="/contactUs" element={<ContactUs />} />
+            <Route path="/ticket/submit" element={<SubmitTicketPage />} />
+            <Route path="/support/chatbot" element={<Chatbot />} />
+            <Route path="/ticket/mytickets" element={<MyTicketPage />} />
+          </Routes>
+          <Footer />
+        </div>
       </QueryClientProvider>
     </MantineProvider>
   );
