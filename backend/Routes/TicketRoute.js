929637249f0ff8185d70c78ad40d95838acc13f4
UdeeshaRukshan <IT21807480@my.sliit.lk>
2023-09-29 03:48:55 +0530

    yes

--- backend/Routes/TicketRoute.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
- 
-const { ticketadd, getAllTickets } = require("../Controllers/TicketController");
-
-router.post("/add", ticketadd);
-router.get("/getall", getAllTickets)
-module.exports = router;
