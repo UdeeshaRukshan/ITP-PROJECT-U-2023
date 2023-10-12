68518bfde51c3ab4e366ff9e989a9daf206ea661
Adithyaranawaka77 <125335079+Adithyaranawaka77@users.noreply.github.com>
2023-10-12 21:04:08 +0530

    some validation

--- frontend/src/pages/CreateForum.js
+++ frontend/src/pages/CreateForum.js
@@ -4,31 +4,61 @@ import Spinner from "../components/Spinner";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { useSnackbar } from "notistack";
-import "./CreateForum.css"; // Update CSS file name if necessary
+import "./CreateForum.css";
 
 const CreateForum = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
-  const [publishYear, setPublishYear] = useState("");
-  const [content, setContent] = useState(""); // Added content state
+  const [content, setContent] = useState("");
   const [loading, setLoading] = useState(false);
+  const [errors, setErrors] = useState({});
   const navigate = useNavigate();
   const { enqueueSnackbar } = useSnackbar();
+  const [publishYear, setPublishYear] = useState(null);
 
   const handleSaveForum = () => {
+    // Reset previous errors
+    setErrors({});
+
+    const validationErrors = {};
+
+    if (!title.trim()) {
+      validationErrors.title = "Title is required";
+    }
+
+    if (!author.trim()) {
+      validationErrors.author = "Author is required";
+    } else if (!/^[A-Za-z\s]+$/.test(author)) {
+      validationErrors.author = "Author can only contain letters and spaces";
+    }
+
+    if (!publishYear.trim() || isNaN(publishYear) || +publishYear < 0) {
+      validationErrors.publishYear = "Invalid publish year";
+    }
+
+    if (!content.trim()) {
+      validationErrors.content = "Content is required";
+    }
+
+    if (Object.keys(validationErrors).length > 0) {
+      setErrors(validationErrors);
+      return;
+    }
+
     const data = {
       title,
       author,
       publishYear,
-      content, // Include content in the data object
+      content,
     };
+
     setLoading(true);
     axios
-      .post("http://localhost:4000/forums", data) // Updated URL to "/forums"
+      .post("http://localhost:4000/forums", data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar("Forum Created successfully", { variant: "success" });
-        navigate("/"); // Assuming you want to navigate back to the homepage
+        navigate("/");
       })
       .catch((error) => {
         setLoading(false);
@@ -51,6 +81,7 @@ const CreateForum = () => {
             onChange={(e) => setTitle(e.target.value)}
             className="border-2 border-gray-500 px-4 py-2 w-full"
           />
+          {errors.title && <p className="text-red-500">{errors.title}</p>}
         </div>
         <div className="my-4">
           <label className="text-xl mr-4 text-gray-500">Author</label>
@@ -60,23 +91,26 @@ const CreateForum = () => {
             onChange={(e) => setAuthor(e.target.value)}
             className="border-2 border-gray-500 px-4 py-2 w-full"
           />
+          {errors.author && <p className="text-red-500">{errors.author}</p>}
         </div>
         <div className="my-4">
           <label className="text-xl mr-4 text-gray-500">Publish Year</label>
           <input
-            type="number"
+            type="text"
             value={publishYear}
             onChange={(e) => setPublishYear(e.target.value)}
             className="border-2 border-gray-500 px-4 py-2 w-full"
           />
+          {errors.publishYear && <p className="text-red-500">{errors.publishYear}</p>}
         </div>
         <div className="my-4">
           <label className="text-xl mr-4 text-gray-500">Content</label>
           <textarea
             value={content}
             onChange={(e) => setContent(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2 w-full h-48" // Adjust the height as needed
+            className="border-2 border-gray-500 px-4 py-2 w-full h-48"
           />
+          {errors.content && <p className="text-red-500">{errors.content}</p>}
         </div>
         <button className="p-2 bg-sky-300 m-8" onClick={handleSaveForum}>
           Save
@@ -85,5 +119,4 @@ const CreateForum = () => {
     </div>
   );
 };
-
 export default CreateForum;
