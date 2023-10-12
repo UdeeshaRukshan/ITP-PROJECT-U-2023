72753e3ce65415b502e363c3c60cda0930e60ff8
UdeeshaRukshan <IT21807480@my.sliit.lk>
2023-10-12 22:18:48 +0530

    update files

--- frontend/src/components/payment/EditCard.js
+++ frontend/src/components/payment/EditCard.js
@@ -12,7 +12,7 @@ const EditCard = () => {
   // Fetch card details from API
   useEffect(() => {
     axios
-      .get("http://localhost:8070/payment/getpayments")
+      .get("http://localhost:4042/payment/getpayments")
       .then((response) => {
         setCards(response.data); // Assuming API returns an array of payment data
       })
@@ -26,71 +26,75 @@ const EditCard = () => {
     setEditCard({ ...cards[index], index });
   };
 
-// Function to handle updating a card with validations
-const handleUpdateCard = () => {
-  // Validate card number length
-  if (editCard.cardNumber.replace(/\s/g, '').length !== 16) {
-    alert("Card number must be 16 digits");
-    return;
-  }
+  // Function to handle updating a card with validations
+  const handleUpdateCard = () => {
+    // Validate card number length
+    if (editCard.cardNumber.replace(/\s/g, "").length !== 16) {
+      alert("Card number must be 16 digits");
+      return;
+    }
 
-  // Validate expiry date format and check if it's not expired
-  const currentDate = new Date();
-  const [expMonth, expYear] = editCard.expiryDate.split("/");
-  const expiryDate = new Date(`20${expYear}`, expMonth - 1);
+    // Validate expiry date format and check if it's not expired
+    const currentDate = new Date();
+    const [expMonth, expYear] = editCard.expiryDate.split("/");
+    const expiryDate = new Date(`20${expYear}`, expMonth - 1);
 
-  if (
-    !/^(0[1-9]|1[0-2])\/\d{2}$/.test(editCard.expiryDate) ||
-    expiryDate < currentDate
-  ) {
-    alert("Please enter a valid expiry date in MM/YY format");
-    return;
-  }
+    if (
+      !/^(0[1-9]|1[0-2])\/\d{2}$/.test(editCard.expiryDate) ||
+      expiryDate < currentDate
+    ) {
+      alert("Please enter a valid expiry date in MM/YY format");
+      return;
+    }
 
-  // Validate CVV length
-  if (editCard.cvv.length !== 3) {
-    alert("CVV must be 3 digits");
-    return;
-  }
+    // Validate CVV length
+    if (editCard.cvv.length !== 3) {
+      alert("CVV must be 3 digits");
+      return;
+    }
 
-  // Format card number with spaces
-  const formattedCardNumber = editCard.cardNumber.replace(/\s/g, '').replace(/(.{4})/g, "$1 ");
+    // Format card number with spaces
+    const formattedCardNumber = editCard.cardNumber
+      .replace(/\s/g, "")
+      .replace(/(.{4})/g, "$1 ");
 
-  axios
-    .put(`http://localhost:8070/payment/updatepayment/${editCard._id}`, {
-      ...editCard,
-      cardNumber: formattedCardNumber.trim(),
-    })
-    .then((response) => {
-      const updatedCard = response.data;
-      const updatedCards = cards.map((c, index) =>
-        index === editCard.index ? updatedCard : c
-      );
-      setCards(updatedCards);
-      setEditCard(null); // Clear the editing state
-    })
-    .catch((err) => {
-      alert(err.message);
-    });
-};
-
-// Function to handle deleting a card with confirmation
-const handleDeleteCard = (index) => {
-  const confirmDelete = window.confirm("Do you want to delete this card?");
-
-  if (confirmDelete) {
     axios
-      .delete(`http://localhost:8070/payment/deletepayment/${cards[index]._id}`)
-      .then(() => {
-        const updatedCards = cards.filter((_, i) => i !== index);
+      .put(`http://localhost:4042/payment/updatepayment/${editCard._id}`, {
+        ...editCard,
+        cardNumber: formattedCardNumber.trim(),
+      })
+      .then((response) => {
+        const updatedCard = response.data;
+        const updatedCards = cards.map((c, index) =>
+          index === editCard.index ? updatedCard : c
+        );
         setCards(updatedCards);
         setEditCard(null); // Clear the editing state
       })
       .catch((err) => {
         alert(err.message);
       });
-  }
-};
+  };
+
+  // Function to handle deleting a card with confirmation
+  const handleDeleteCard = (index) => {
+    const confirmDelete = window.confirm("Do you want to delete this card?");
+
+    if (confirmDelete) {
+      axios
+        .delete(
+          `http://localhost:8070/payment/deletepayment/${cards[index]._id}`
+        )
+        .then(() => {
+          const updatedCards = cards.filter((_, i) => i !== index);
+          setCards(updatedCards);
+          setEditCard(null); // Clear the editing state
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    }
+  };
 
   return (
     <div className="edit-card-container">
@@ -112,7 +116,8 @@ const handleDeleteCard = (index) => {
               <td>{index + 1}</td>
               <td>
                 {editCard && editCard.index === index ? (
-                  <input className="edit-card-input"
+                  <input
+                    className="edit-card-input"
                     type="text"
                     value={editCard.cardName}
                     onChange={(e) =>
@@ -125,7 +130,8 @@ const handleDeleteCard = (index) => {
               </td>
               <td>
                 {editCard && editCard.index === index ? (
-                  <input className="edit-card-input"
+                  <input
+                    className="edit-card-input"
                     type="text"
                     value={editCard.cardNumber}
                     onChange={(e) =>
@@ -138,7 +144,8 @@ const handleDeleteCard = (index) => {
               </td>
               <td>
                 {editCard && editCard.index === index ? (
-                  <input className="edit-card-input"
+                  <input
+                    className="edit-card-input"
                     type="text"
                     value={editCard.expiryDate}
                     onChange={(e) =>
@@ -151,7 +158,8 @@ const handleDeleteCard = (index) => {
               </td>
               <td>
                 {editCard && editCard.index === index ? (
-                  <input className="edit-card-input"
+                  <input
+                    className="edit-card-input"
                     type="text"
                     value={editCard.cvv}
                     onChange={(e) =>
@@ -165,7 +173,10 @@ const handleDeleteCard = (index) => {
               <td>
                 {editCard && editCard.index === index ? (
                   <>
-                    <button className="save-button-editcard" onClick={handleUpdateCard}>
+                    <button
+                      className="save-button-editcard"
+                      onClick={handleUpdateCard}
+                    >
                       Save
                     </button>
                     <button
