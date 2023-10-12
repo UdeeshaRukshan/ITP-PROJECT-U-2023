62ddfa557a6002aac2462ebd331a2198378ed018
Nuwani25 <dnuwanid@gmail.com>
2023-10-12 22:13:45 +0530

    Edit done with navigating

--- frontend/src/components/AllArts.js
+++ frontend/src/components/AllArts.js
@@ -1,19 +1,16 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import "./AllArts.css"; 
-import EditArtForm from "./EditArtForm"; // Replace "./EditArtForm" with the correct path to your EditArtForm component file.
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
+import './AllArts.css';
 
 export default function AllArts() {
-  // State to store art data
   const [arts, setArts] = useState([]);
-  const [editingArt, setEditingArt] = useState(null);
+  const [editingArtId, setEditingArtId] = useState(null);
 
-  // Fetch art data from the server
   useEffect(() => {
     async function fetchArts() {
       try {
-        const response = await axios.get("http://localhost:8070/art/getarts");
+        const response = await axios.get('http://localhost:8070/art/getarts');
         setArts(response.data);
       } catch (error) {
         alert(error.message);
@@ -23,17 +20,20 @@ export default function AllArts() {
     fetchArts();
   }, []);
 
-  const renderImages = (images) => {
-    return images.map((image, index) => (
-      <img
-        key={index}
-        src={image.dataUrl}
-        alt={`Art ${index + 1}`}
-      />
-    ));
+  const handleDeleteArt = async (artId) => {
+    const confirmDelete = window.confirm('Are you sure you want to delete this art piece?');
+
+    if (confirmDelete) {
+      try {
+        await axios.delete(`http://localhost:8070/art/deleteart/${artId}`);
+        // Update the state to remove the deleted art
+        setArts((prevArts) => prevArts.filter((art) => art._id !== artId));
+      } catch (error) {
+        alert(error.message);
+      }
+    }
   };
 
-  
   const renderArtsRows = () => {
     return arts.map((art) => (
       <tr key={art._id}>
@@ -44,63 +44,22 @@ export default function AllArts() {
         <td>{art.condition}</td>
         <td>{art.location}</td>
         <td>{art.value}</td>
+        <td>{art.images}</td>
         <td>
-        <td>{renderImages(art.images)}</td>
-      </td>
-      <td>
-        <div className="all-arts-button-container">
-        <button
-              className="all-arts-edit-button"
-              onClick={() => handleEditClick(art)}
-            >
-              Edit
-            </button>
-          <button className="delete-button-allarts" onClick={() => handleDeleteClick(art._id)}>
-            Delete
-          </button>
-        </div>
-      </td>
+  <div className="all-arts-button-container">
+    <Link to={`/updateart/${art._id}`}>
+      <button className="all-arts-edit-button">Edit</button>
+    </Link>
+    <button className="delete-button-allarts" onClick={() => handleDeleteArt(art._id)}>Delete</button>
+  </div>
+</td>
       </tr>
     ));
   };
 
-  const handleUpdateArt = async (updatedArt) => {
-    try {
-      await axios.put(`http://localhost:8070/art/updateart/${updatedArt._id}`, updatedArt);
-      setArts((prevArts) =>
-        prevArts.map((art) =>
-          art._id === updatedArt._id ? updatedArt : art
-        )
-      );
-      setEditingArt(null); // Close the edit form/modal
-    } catch (error) {
-      alert(error.message);
-    }
-  };
-
-  // Function to handle delete button click
-  const handleDeleteClick = async (artId) => {
-    const confirmDelete = window.confirm("Are you sure you want to delete this art piece?");
-    
-    if (confirmDelete) {
-      try {
-        await axios.delete(`http://localhost:8070/art/deleteart/${artId}`);
-
-        setArts((prevArts) => prevArts.filter((art) => art._id !== artId));
-      } catch (error) {
-        alert(error.message);
-      }
-    }
-  };
-
-  const handleEditClick = (art) => {
-    setEditingArt(art);
-  };
-  
-
   return (
     <div className="all-arts-container">
-      <h2 className="all-arts-header">All Arts</h2>
+      <h1 className="all-arts-header-center">All Arts</h1>
       <table className="all-arts-table">
         <thead className="all-arts-thread">
           <tr className="tr-all-arts">
@@ -111,19 +70,12 @@ export default function AllArts() {
             <th className="all-arts-th">Condition</th>
             <th className="all-arts-th">Location</th>
             <th className="all-arts-th">Value($)</th>
-            <th className="all-arts-th">Images</th> 
+            <th className="all-arts-th">Images</th>
             <th className="all-arts-th">Actions</th>
           </tr>
         </thead>
         <tbody className="all-arts-tbody">{renderArtsRows()}</tbody>
       </table>
-      {editingArt && (
-        <EditArtForm
-          art={editingArt}
-          onUpdateArt={handleUpdateArt}
-          onCancel={() => setEditingArt(null)}
-        />
-      )}
     </div>
   );
 }
--- frontend/src/components/EditArtForm.js
+++ frontend/src/components/EditArtForm.js
@@ -1,17 +1,43 @@
-import React, { useState } from "react";
-import "./EditArtForm.css";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom'; // Import useParams
+import './EditArtForm.css';
 
-export default function EditArtForm({ art, onUpdateArt, onCancel }) {
-  const [updatedArt, setUpdatedArt] = useState(art);
+export default function EditArtForm() {
+  const { artid } = useParams(); // Get the art ID from the URL parameter
+  const [updatedArt, setUpdatedArt] = useState({});
+
+  useEffect(() => {
+    // Fetch art data for editing based on the art ID
+    async function fetchArtData() {
+      try {
+        const response = await axios.get(`http://localhost:8070/art/getarts/${artid}`);
+        setUpdatedArt(response.data.Art);
+      } catch (error) {
+        alert(error.message);
+      }
+    }
+
+    fetchArtData();
+  }, [artid]);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setUpdatedArt({ ...updatedArt, [name]: value });
   };
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    onUpdateArt(updatedArt);
+    try {
+      await axios.put(`http://localhost:8070/art/updateart/${artid}`, updatedArt);
+      window.location.href = '/getarts'; // Redirect to the list of arts after successful update
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const handleCancel = () => {
+    window.location.href = '/getarts'; // Redirect to the list of arts without saving changes
   };
 
   return (
@@ -23,7 +49,7 @@ export default function EditArtForm({ art, onUpdateArt, onCancel }) {
           <textarea
             name="title"
             className="edit-art-form-textarea"
-            placeholder="e.g., Starry night by vincent van gogh"
+            placeholder="e.g., Starry night by Vincent van Gogh"
             required
             value={updatedArt.title}
             onChange={handleInputChange}
@@ -34,30 +60,25 @@ export default function EditArtForm({ art, onUpdateArt, onCancel }) {
           <input
             type="text"
             name="medium"
-            className="art-form-input"
-            placeholder="e.g., canvas,wood,clay,paint etc."
+            className="edit-art-form-input"
+            placeholder="e.g., canvas, wood, clay, paint, etc."
             required
             value={updatedArt.medium}
             onChange={handleInputChange}
           />
         </label>
-
-        <div className="edit-form-row">
-          <div className="edit-form-col"></div>
         <label className="edit-art-form-label">
           Height (cm):
           <input
             type="number"
             name="height"
-            className="art-form-input"
+            className="edit-art-form-input"
             placeholder="e.g., 120"
             required
             value={updatedArt.height}
             onChange={handleInputChange}
           />
         </label>
-        </div>
-        <div className="edit-form-col">
         <label className="edit-art-form-label">
           Width (cm):
           <input
@@ -70,10 +91,8 @@ export default function EditArtForm({ art, onUpdateArt, onCancel }) {
             onChange={handleInputChange}
           />
         </label>
-        </div>
         <label className="edit-art-form-label">
-        Describe the condition of the art:
-        </label>
+          Describe the condition of the art:
           <textarea
             name="condition"
             className="edit-art-form-textarea"
@@ -82,44 +101,45 @@ export default function EditArtForm({ art, onUpdateArt, onCancel }) {
             value={updatedArt.condition}
             onChange={handleInputChange}
           />
+        </label>
         <label className="edit-art-form-label">
           Location:
-          </label>
           <select
-            type="text"
             name="location"
             required
             value={updatedArt.location}
             onChange={handleInputChange}
           >
-          <option value="">Select Location</option>
-          <option value="Colombo">Colombo</option>
-          <option value="Gampaha">Gampaha</option>
-          <option value="Kaduwela">Kaduwela</option>
-          <option value="Ratnapura">Ratnapura</option>
-          <option value="Trincomalee">Trincomalee</option>
-          <option value="Polonnaruwa">Polonnaruwa</option>
-          <option value="Anuradhapura">Anuradhapura</option>
-        </select>
-        
+            <option value="">Select Location</option>
+            <option value="Colombo">Colombo</option>
+            <option value="Gampaha">Gampaha</option>
+            <option value="Kaduwela">Kaduwela</option>
+            <option value="Ratnapura">Ratnapura</option>
+            <option value="Trincomalee">Trincomalee</option>
+            <option value="Polonnaruwa">Polonnaruwa</option>
+            <option value="Anuradhapura">Anuradhapura</option>
+          </select>
+        </label>
         <label className="edit-art-form-label">
           Value ($):
-          </label>
           <input
             type="number"
             name="value"
-            className="art-form-input"
+            className="edit-art-form-input"
             placeholder="e.g., 120"
+            required
             value={updatedArt.value}
             onChange={handleInputChange}
           />
-       
-       <div className="edit-art-form-button-container">
-  <button type="submit" className="edit-art-form-save-button">Save</button>
-  <button type="button" className="edit-art-form-cancel-button" onClick={onCancel}>
-    Cancel
-  </button>
-</div>
+        </label>
+        <div className="edit-art-form-button-container">
+          <button type="submit" className="edit-art-form-save-button">
+            Save
+          </button>
+          <a href="/getarts" className="edit-art-form-cancel-button">
+            Cancel
+          </a>
+        </div>
       </form>
     </div>
   );
