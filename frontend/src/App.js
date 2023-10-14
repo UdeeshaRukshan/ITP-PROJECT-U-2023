c13f7f659f5553973d6fe8085338cfb6c5d98dd7
UdeeshaRukshan <IT21807480@my.sliit.lk>
2023-10-14 20:22:56 +0530

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,24 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import Artical from "./pages/Artical";
 import CreateForum from "./pages/CreateForum"; // Import the appropriate component for creating forums
 import ShowForum from "./pages/ShowForum"; // Import the appropriate component for displaying a single forum
 import EditForum from "./pages/EditForum"; // Import the appropriate component for editing forums
 import DeleteForum from "./pages/DeleteForum"; // Import the appropriate component for deleting forums
 
-
-
 const App = () => {
   return (
-    
-     
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/forums/create" element={<CreateForum />} /> {/* Route for creating a new forum */}
-      <Route path="/forums/:id" element={<ShowForum />} /> {/* Route for displaying a single forum */}
-      <Route path="/forums/edit/:id" element={<EditForum />} /> {/* Route for editing a forum */}
-      <Route path="/forums/delete/:id" element={<DeleteForum />} /> {/* Route for deleting a forum */}
+      <Route path="/" element={<Artical />} />
+      <Route path="/forums/create" element={<CreateForum />} />{" "}
+      {/* Route for creating a new forum */}
+      <Route path="/forums/:id" element={<ShowForum />} />{" "}
+      {/* Route for displaying a single forum */}
+      <Route path="/forums/edit/:id" element={<EditForum />} />{" "}
+      {/* Route for editing a forum */}
+      <Route path="/forums/delete/:id" element={<DeleteForum />} />{" "}
+      {/* Route for deleting a forum */}
     </Routes>
-    
   );
 };
 
